84b08581555fdcea9d22cb9fffdcb4edfa13a473
Hilda Afifah <103301400+hiafa1472@users.noreply.github.com>
2023-06-14 19:29:58 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SettingTime = New System.Windows.Forms.DateTimePicker()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Result = New System.Windows.Forms.RichTextBox()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.SettingTime)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TextBox1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label2)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Result)
+        Me.SplitContainer1.Size = New System.Drawing.Size(800, 450)
+        Me.SplitContainer1.SplitterDistance = 266
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(119, 150)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(105, 32)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "hitung "
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'SettingTime
+        '
+        Me.SettingTime.CustomFormat = "dd/MM/yy HH:mm:ss"
+        Me.SettingTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.SettingTime.Location = New System.Drawing.Point(69, 94)
+        Me.SettingTime.Name = "SettingTime"
+        Me.SettingTime.Size = New System.Drawing.Size(155, 20)
+        Me.SettingTime.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(69, 49)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(124, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 100)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Tanggal"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nama"
+        '
+        'Result
+        '
+        Me.Result.BackColor = System.Drawing.Color.RosyBrown
+        Me.Result.Location = New System.Drawing.Point(7, 7)
+        Me.Result.Name = "Result"
+        Me.Result.Size = New System.Drawing.Size(522, 442)
+        Me.Result.TabIndex = 0
+        Me.Result.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents SettingTime As DateTimePicker
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Result As RichTextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim tgl_sekarang As Date
+        Dim tgl_lahir As Date
+
+        tgl_sekarang = Now.Date
+        tgl_lahir = SettingTime.Value
+
+        Dim usia_tahun As Integer
+        Dim usia_bulan As Integer
+        Dim usia_hari As Integer
+        Dim usia_jam As Integer
+        usia_tahun = tgl_sekarang.Year - tgl_lahir.Year
+        usia_bulan = tgl_sekarang.Month - tgl_lahir.Month
+        usia_hari = tgl_lahir.Day - tgl_sekarang.Day
+        usia_jam = tgl_sekarang.Day
+        If usia_bulan < 0 Then
+            usia_bulan = usia_bulan + 12
+            usia_tahun = usia_tahun - 1
+            usia_hari = usia_hari + 365
+
+        End If
+        If tgl_lahir.Year < tgl_sekarang.Year Then
+            usia_bulan = tgl_lahir.Month - tgl_sekarang.Month
+            usia_hari = tgl_lahir.Day - tgl_sekarang.Day
+
+        End If
+        If tgl_lahir.Month < tgl_sekarang.Month Then
+            usia_hari = tgl_lahir.Day - tgl_sekarang.Day
+
+        End If
+
+        Select Case True
+            Case usia_bulan = 1 And usia_tahun = 0 And usia_hari < 31
+                usia_bulan = 0
+        End Select
+        Dim hasil_usia As String
+        hasil_usia = $"Nama saya :  {TextBox1.Text}
+                       Tanggal lahir : {tgl_lahir}
+                       Saat ini kamu berusia : {usia_tahun} Tahun {usia_bulan} Bulan {usia_hari} Hari"
+        Result.Text = hasil_usia
+    End Sub
+End Class
